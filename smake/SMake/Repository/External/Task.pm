c2f859b1176e4198cbb57d069c3c0b7d66b9d43c
ondrej <ondrej@ondra-lenovo>
2014-01-03 17:26:02 +0100

    copyright and licensing notes

--- smake/SMake/Repository/External/Task.pm
+++ smake/SMake/Repository/External/Task.pm
@@ -1,3 +1,20 @@
+# Copyright (C) 2014 Aveco s.r.o.
+#
+# This file is part of SMake.
+#
+# SMake is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SMake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SMake.  If not, see <http://www.gnu.org/licenses/>.
+
 # Implementation of the task object for the external repository
 package SMake::Repository::External::Task;
 
